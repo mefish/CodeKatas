2f34483eb8ce6fcea07033721caf912a00eccd66
Matthew Fisher <mefisher@Matthews-MacBook-Pro.local>
2017-02-28 11:07:56 -0600

    New initial commit

---
+++ DataMunging/src/main/java/com/mefish/katas/datamunging/DataMungingApplication.java
@@ -0,0 +1,12 @@
+package com.mefish.katas.datamunging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMungingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMungingApplication.class, args);
+	}
+}
---
+++ DataMunging/src/test/java/com/mefish/katas/datamunging/DataMungingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mefish.katas.datamunging;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataMungingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
